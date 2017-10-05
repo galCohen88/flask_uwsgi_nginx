abe4d54949a0ea233ee777275961735ce08f8e6a
nevix <gal. nevis@gmail.com>
2017-10-05 11:45:34 +0300

    server

--- drone/server.py
+++ drone/server.py
@@ -16,3 +16,23 @@ def listen():
         sensors = [sensor1, sensor2, sensor3]
         detection_sensors = [i for i, x in enumerate(sensors) if x.mode == SENSOR_MODE_PASSIVE_TRACKING]
 
+        for idx in detection_sensors:
+            if is_a_threat(sensors[idx]):
+                handle_threat()
+
+def handle_threat():
+    
+def choose_sensor(all_sensors_detected_the_threat):
+    "policy - the active sensor is the closest one to the base - taking over only once"
+    "at what stage should the sensor take over? when the drone is considered a threat"
+
+
+def is_a_threat(sensor):
+    # return True if distance & speed & bearing is calculated to threat on compound
+
+    drone_distance_from_sensor = sensor.calculate_distance_from_object(sensor.tracked_drone_movement[-1])
+    velocity_vector = sensor.velocity_vector
+
+    if 'bearing and speed considered a threat':
+        return True
+    return False
